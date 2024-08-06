0ee9c5a91fe0d61f936dfd67ecccb4f9babd3838
neyneyalldayday <drfrankenstein123@gmail.com>
2024-08-06 12:57:31 -0500

    server file

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 import express from 'express';
 dotenv.config();
 
+
 // Import the routes
 import routes from './routes/index.js';
 
@@ -10,6 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 // TODO: Serve static files of entire client dist folder
+app.use(express.static('../client/dist'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 // TODO: Implement middleware for parsing JSON and urlencoded form data
 
